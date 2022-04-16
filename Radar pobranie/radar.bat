b3b49aa57279c29b79e51775d15154aa57dc2a07
brzoskwinia <103823812+brzoskwinia@users.noreply.github.com>
2022-04-16 12:23:10 +0200

    Add files via upload

---
+++ Radar pobranie/radar.bat
@@ -0,0 +1,33 @@
+echo Pobieranie danych radarowych ...
+mkdir C:\Users\Pawlowscy\Desktop\radar
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p030.gif C:\Users\Pawlowscy\Desktop\radar\p000.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p001.gif C:\Users\Pawlowscy\Desktop\radar\p001.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p002.gif C:\Users\Pawlowscy\Desktop\radar\p002.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p003.gif C:\Users\Pawlowscy\Desktop\radar\p003.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p004.gif C:\Users\Pawlowscy\Desktop\radar\p004.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p005.gif C:\Users\Pawlowscy\Desktop\radar\p005.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p006.gif C:\Users\Pawlowscy\Desktop\radar\p006.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p007.gif C:\Users\Pawlowscy\Desktop\radar\p007.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p008.gif C:\Users\Pawlowscy\Desktop\radar\p008.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p009.gif C:\Users\Pawlowscy\Desktop\radar\p009.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p010.gif C:\Users\Pawlowscy\Desktop\radar\p010.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p011.gif C:\Users\Pawlowscy\Desktop\radar\p011.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p012.gif C:\Users\Pawlowscy\Desktop\radar\p012.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p013.gif C:\Users\Pawlowscy\Desktop\radar\p013.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p014.gif C:\Users\Pawlowscy\Desktop\radar\p014.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p015.gif C:\Users\Pawlowscy\Desktop\radar\p015.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p016.gif C:\Users\Pawlowscy\Desktop\radar\p016.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p017.gif C:\Users\Pawlowscy\Desktop\radar\p017.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p018.gif C:\Users\Pawlowscy\Desktop\radar\p018.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p019.gif C:\Users\Pawlowscy\Desktop\radar\p019.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p020.gif C:\Users\Pawlowscy\Desktop\radar\p020.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p021.gif C:\Users\Pawlowscy\Desktop\radar\p021.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p022.gif C:\Users\Pawlowscy\Desktop\radar\p022.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p023.gif C:\Users\Pawlowscy\Desktop\radar\p023.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p024.gif C:\Users\Pawlowscy\Desktop\radar\p024.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p025.gif C:\Users\Pawlowscy\Desktop\radar\p025.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p026.gif C:\Users\Pawlowscy\Desktop\radar\p026.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p027.gif C:\Users\Pawlowscy\Desktop\radar\p027.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p028.gif C:\Users\Pawlowscy\Desktop\radar\p028.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p029.gif C:\Users\Pawlowscy\Desktop\radar\p029.gif
+pause
---
+++ Radar pobranie/radar_uni.bat
@@ -0,0 +1,33 @@
+echo Pobieranie danych radarowych ...
+mkdir %USERPROFILE%\Desktop\radar
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p030.gif %USERPROFILE%\Desktop\radar\p000.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p001.gif %USERPROFILE%\Desktop\radar\p001.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p002.gif %USERPROFILE%\Desktop\radar\p002.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p003.gif %USERPROFILE%\Desktop\radar\p003.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p004.gif %USERPROFILE%\Desktop\radar\p004.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p005.gif %USERPROFILE%\Desktop\radar\p005.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p006.gif %USERPROFILE%\Desktop\radar\p006.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p007.gif %USERPROFILE%\Desktop\radar\p007.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p008.gif %USERPROFILE%\Desktop\radar\p008.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p009.gif %USERPROFILE%\Desktop\radar\p009.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p010.gif %USERPROFILE%\Desktop\radar\p010.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p011.gif %USERPROFILE%\Desktop\radar\p011.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p012.gif %USERPROFILE%\Desktop\radar\p012.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p013.gif %USERPROFILE%\Desktop\radar\p013.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p014.gif %USERPROFILE%\Desktop\radar\p014.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p015.gif %USERPROFILE%\Desktop\radar\p015.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p016.gif %USERPROFILE%\Desktop\radar\p016.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p017.gif %USERPROFILE%\Desktop\radar\p017.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p018.gif %USERPROFILE%\Desktop\radar\p018.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p019.gif %USERPROFILE%\Desktop\radar\p019.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p020.gif %USERPROFILE%\Desktop\radar\p020.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p021.gif %USERPROFILE%\Desktop\radar\p021.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p022.gif %USERPROFILE%\Desktop\radar\p022.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p023.gif %USERPROFILE%\Desktop\radar\p023.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p024.gif %USERPROFILE%\Desktop\radar\p024.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p025.gif %USERPROFILE%\Desktop\radar\p025.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p026.gif %USERPROFILE%\Desktop\radar\p026.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p027.gif %USERPROFILE%\Desktop\radar\p027.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p028.gif %USERPROFILE%\Desktop\radar\p028.gif
+httpget http://awiacja.imgw.pl/mapa-burz-rad/_burzrad_p3/p029.gif %USERPROFILE%\Desktop\radar\p029.gif
+pause
